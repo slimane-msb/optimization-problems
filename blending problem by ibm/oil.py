a8039dcd654e4ededece63299d6acc91fa20e572
slimane-msb <slimanemesbah0@gmail.com>
2022-11-12 22:40:28 +0100

    adding objective function

--- blending problem by ibm/oil.py
+++ blending problem by ibm/oil.py
@@ -2,7 +2,7 @@ import pulp
 
 print ("oil")
 
-def optimize_me(crudes, gasolines, costs, subs):
+def optimize_me(crudes, gasolines, costs, subs, prod):
     # variable names: 
     var_names =  [crude["name"] for crude in crudes]
     var_names += [gasoline["name"] for gasoline in gasolines]
@@ -10,7 +10,48 @@ def optimize_me(crudes, gasolines, costs, subs):
         var_names += [gasoline["name"]+crude["name"] for crude in crudes]
     var_names += ["a"+gasoline["name"] for gasoline in gasolines]
 
-    print(var_names)
+    # variables: 
+    variables = pulp.LpVariable.dicts(
+        "Var", var_names, lowBound=0, cat=pulp.LpInteger
+    )
+    prob = pulp.LpProblem("faster_pb", pulp.LpMaximize)
+
+
+    # contraintes: 
+        # alpha: 
+    for crude in crudes: 
+        prob += pulp.lpSum(variables[crude["name"]] ) <=  crude["max"]
+
+        # beta : 
+    for gasoline in gasolines: 
+        prob += pulp.lpSum(variables[gasoline["name"]] ) >= gasoline["max"]+prod["ads_incr"]*var_names["a"+gasoline["name"]]
+
+        # gama : 
+    prob += pulp.lpSum(variables[crude["name"]] for crude in crudes ) <=  prod["max_prod"]
+
+        # sigma o: sup
+    for sub in subs:
+        for gasoline in gasolines: 
+            prob += pulp.lpSum(sub["dir"]*crude[sub["name"]]*var_names[gasoline["name"]+crude["name"]] for crude in crudes ) >=  sub["dir"]*gasoline[sub["name"]]
+
+    # fonction objective 
+        # sell 
+    prob += pulp.lpSum( gasoline["prix"]*variables[gasoline["name"]]  for gasoline in gasolines )
+        # purchase 
+    prob += pulp.lpSum( (-1)*crude["prix"]*variables[crude["name"]]  for crude in crudes )
+        # ads 
+    prob += pulp.lpSum( (-1)*costs["ads"]*variables["a"+gasoline["name"]]  for gasoline in gasolines )
+        # production 
+    prob += pulp.lpSum( (-1)*costs["production"]*variables[crude["name"]]  for crude in crudes )
+
+
+
+    # pulp solve: 
+    prob.solve(pulp.PULP_CBC_CMD(msg=False))
+    for v in [variables[v_name] for v_name in var_names]:
+        print(v, v.value())
+
+
     
 
 optimize_me (
@@ -26,5 +67,9 @@ optimize_me (
         dict ( name = "diesel"   , octane = 6  , lead = 1 , price = 50 , max = 1000) ,
     ] ,
     costs = dict ( ads = 1 , production = 4),
-    subs = ["octane", "lead"]
+    subs = [
+        dict ( name = "octane", dir = 1),
+        dict ( name = "lead"  , dir = -1),
+    ], 
+    prod = dict ( ads_incr = 10, max_prod = 14000 )
 )
